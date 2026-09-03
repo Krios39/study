873093a9b55734c25e2dca146a01869194a55019
ryslan <neshchadimov.ruslan@gmail.com>
2026-09-03 23:00:20 +0300

    finish t2 code

--- 1_semester/algoritmes/hw/hw1/src/bin/T2.rs
+++ 1_semester/algoritmes/hw/hw1/src/bin/T2.rs
@@ -1,47 +1,101 @@
 extern crate rand;
-const ARRAYS_COUNT: usize = 10;
-const ARRAY_SIZE: usize = 5;
-const LARGE_SIZES: [i32; ARRAY_SIZE] = [100, 1_000, 10_000, 100_000, 1_000_000];
+use std::time::Instant;
+const SIZE: usize = 5;
+const LARGE_SIZES: [i32; SIZE] = [100, 1_000, 10_000, 100_000, 1_000_000];
+const QUADRATIC_SIZES: [i32; SIZE] = [1_000, 2_000, 4_000, 8_000, 16_000];
 fn main() {
-    let mut arrays: [[i32; ARRAY_SIZE]; ARRAYS_COUNT] = [[0; ARRAY_SIZE]; ARRAYS_COUNT];
+    println!("LinearScan&Sort");
 
-    for i in 0..ARRAYS_COUNT {
-        arrays[i] = create_random_array()
-    }
+    for size in LARGE_SIZES {
+        let data = create_random_array(size);
 
-    for i in 0..ARRAYS_COUNT {
-        println!("{:?}", arrays[i]);
-    }
+        let linear_start = Instant::now();
+        linear_scan(&data);
+        let liner_time = linear_start.elapsed().as_micros();
 
-    for i in 0..ARRAYS_COUNT {
-        let sorted_array = lib_sort(arrays[i]);
-        linear_scan(sorted_array, sorted_array.len());
+        let sort_start = Instant::now();
+        lib_sort(&data);
+        let sort_time = sort_start.elapsed().as_micros();
+
+        println!("{:<10} | {:<18.4} | {:<18.4}", size, liner_time, sort_time);
     }
-    lib_sort(LARGE_SIZES);
+    println!("Quadratic");
+
+    for size in QUADRATIC_SIZES {
+        let data = create_random_array(size);
+
+        let quadratic_start = Instant::now();
+        quadratic_inversion_counter(&data);
+        let quadratic_time = quadratic_start.elapsed().as_micros();
+
+        println!("{:<10} | {:<18.4}", size, quadratic_time,);
+    }
 }
 
-
-fn create_random_array()-> [i32; ARRAY_SIZE] {
-    let mut array: [i32; ARRAY_SIZE] = [0; ARRAY_SIZE];
-
-    for num in 0..ARRAY_SIZE {
-        array[num] = rand::random_range(0..LARGE_SIZES[rand::random_range(0..LARGE_SIZES.len())]);
-    }
+fn create_random_array(elements_count: i32) -> Vec<i32> {
+    let array: Vec<i32> = (0..elements_count).map(|_| rand::random()).collect();
     array
 }
 
-fn linear_scan(array: [i32; ARRAY_SIZE], array_size: usize)  {
-    let mut counter: i32 = 0;
-    for num in 0..(array_size - 1) {
-        counter += (array[num + 1] - array[num]);
+fn linear_scan(array: &Vec<i32>) -> i64 {
+    let mut counter: i64 = 0;
+    for num in 0..(array.len() - 1) {
+        counter += (array[num + 1] as i64) - (array[num] as i64);
     }
-    println!("Counter count {counter} ",);
+    counter
 }
 
-fn lib_sort(array: [i32; ARRAY_SIZE])->[i32; ARRAY_SIZE] {
+fn lib_sort(array: &Vec<i32>) -> Vec<i32> {
     let mut copy = array.clone();
     copy.sort();
     copy
 }
 
-fn quadratic_inversion_counter() {}
+fn quadratic_inversion_counter(array: &[i32]) -> i32 {
+    let mut counter = 0;
+    let n = array.len();
+
+    for i in 0..n {
+        for j in i + 1..n {
+            if i < j && array[i] > array[j] {
+                counter += 1;
+            }
+        }
+    }
+    counter
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_linear_scan() {
+        let data = vec![1, 5, 2]; // vec!, а не [1, 5, 2]
+        assert_eq!(linear_scan(&data), 1);
+    }
+
+    #[test]
+    fn test_lib_sort() {
+        let data = vec![5, 1, 4, 2];
+        assert_eq!(lib_sort(&data), vec![1, 2, 4, 5]);
+    }
+
+    #[test]
+    fn test_quadratic_inversions_manual() {
+        let data = [4, 1, 3, 2];
+        assert_eq!(quadratic_inversion_counter(&data), 4);
+    }
+
+    #[test]
+    fn test_quadratic_inversions_sorted() {
+        let data = [1, 2, 3, 4, 5];
+        assert_eq!(quadratic_inversion_counter(&data), 0);
+    }
+
+    #[test]
+    fn test_quadratic_inversions_reversed() {
+        let data = [5, 4, 3, 2, 1];
+        assert_eq!(quadratic_inversion_counter(&data), 10);
+    }
+}
