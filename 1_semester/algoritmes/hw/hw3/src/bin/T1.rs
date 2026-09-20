c0ace9ad7df8964e02324be9b04b2f53936d104e
ryslan <neshchadimov.ruslan@gmail.com>
2026-09-20 13:21:10 +0300

    finish task1

--- 1_semester/algoritmes/hw/hw3/src/bin/T1.rs
+++ 1_semester/algoritmes/hw/hw3/src/bin/T1.rs
@@ -1,23 +1,61 @@
-use hw3::bst::BST;
+
+use hw3::bst::{BST, TreeAnalyzer};
 
 fn main() {
     let colatz7 = collatz_conjecture(7);
     let colatz9 = collatz_conjecture(9);
     let colatz51 = collatz_conjecture(51);
 
-    println!("colatz 7 {:?}", colatz7);
-    println!("colatz 9 {:?}", colatz9);
-    println!("colatz 51 {:?}", colatz51);
+    println!("=== 1. GIVEN SEQUENCES ===");
+    println!("7: {:?}", colatz7);
+    println!("9: {:?}", colatz9);
+    println!("51: {:?}", colatz51);
 
+    let tree7 = BST::from_vec(colatz7);
+    let tree9 = BST::from_vec(colatz9);
+    let tree51 = BST::from_vec(colatz51);
 
-    let longest_colatz = longest_collatz();
-    println!(" longest colatz {:?} and it {:?}", longest_colatz, collatz_conjecture(longest_colatz));
+    let a7 = TreeAnalyzer::new(&tree7);
+    let a9 = TreeAnalyzer::new(&tree9);
+    let a51 = TreeAnalyzer::new(&tree51);
 
-    let tree1 = BST::from_vec(colatz7);
-    let tree2 = BST::from_vec(colatz9);
-    let tree3 = BST::from_vec(colatz51);
-    let tree4 = BST::from_vec(collatz_conjecture(longest_colatz));
+    println!("\n=== 2. COMPACT TREES ===");
+    println!("Tree 7:");
+    tree7.print_compact();
+    println!("Tree 9:");
+    tree9.print_compact();
+    println!("Tree 51:");
+    tree51.print_compact();
+
+    println!("\n=== 3. SEARCH TRACE IN TREE 51 ===");
+    a51.search_with_trace(&16);
+    a51.search_with_trace(&100);
+
+    println!("\n=== 4. FOURTH SEQUENCE SEARCH ===");
+    let longest_start = longest_collatz();
+    let colatz_longest = collatz_conjecture(longest_start);
+    let tree_longest = BST::from_vec(colatz_longest.clone());
+    let a_longest = TreeAnalyzer::new(&tree_longest);
+
+    let max_reached = colatz_longest.iter().max().unwrap();
+
+    println!("Searched interval: 4 ..= 100_004");
+    println!("Selected starting value: {}", longest_start);
+    println!("Sequence length: {}", colatz_longest.len());
+    println!("Largest reached value: {}", max_reached);
+
+    println!("\n=== 5. STRUCTURAL COMPARISON TABLE ===");
+    println!("{:<10} | {:<10} | {:<10} | {:<15} | {:<30}", "Tree", "Node Count", "Height", "Max Width", "Depth Distribution (Successful Search)");
+    println!("{:-<100}", "");
+    println!("{:<10} | {:<10} | {:<10} | {:<15} | {:?}", "7", a7.count_nodes(), a7.height(), a7.max_width(), a7.depth_distribution());
+    println!("{:<10} | {:<10} | {:<10} | {:<15} | {:?}", "9", a9.count_nodes(), a9.height(), a9.max_width(), a9.depth_distribution());
+    println!("{:<10} | {:<10} | {:<10} | {:<15} | {:?}", "51", a51.count_nodes(), a51.height(), a51.max_width(), a51.depth_distribution());
+    println!("{:<10} | {:<10} | {:<10} | {:<15} | See below", longest_start, a_longest.count_nodes(), a_longest.height(), a_longest.max_width());
+
+    println!("\nDepth distribution for Tree {}:", longest_start);
+    println!("{:?}", a_longest.depth_distribution());
 }
+
 
 fn collatz_conjecture(n: i32) -> Vec<i32> {
     let mut v = vec![n];
--- 1_semester/algoritmes/hw/hw3/src/bst.rs
+++ 1_semester/algoritmes/hw/hw3/src/bst.rs
@@ -1,3 +1,6 @@
+use std::collections::VecDeque;
+use std::cmp::max;
+
 pub struct BST<T> {
     pub root: Option<Box<Node<T>>>,
 }
@@ -14,7 +17,7 @@ impl<T: Ord> Node<T> {
     }
 }
 
-impl<T: Ord> BST<T> {
+impl<T: Ord + std::fmt::Debug> BST<T> {
     pub fn new() -> Self {
         Self { root: None }
     }
@@ -110,5 +113,98 @@ impl<T: Ord> BST<T> {
             (node, right)
         }
     }
+
+    pub fn print_compact(&self) {
+        Self::print_rec(&self.root, 0);
+        println!("-------------------");
+    }
+
+    fn print_rec(node: &Option<Box<Node<T>>>, depth: usize) {
+        if let Some(n) = node {
+            Self::print_rec(&n.right, depth + 1);
+
+            println!("{}{:?}", "    ".repeat(depth), n.value);
+
+            Self::print_rec(&n.left, depth + 1);
+        }
+    }
 }
 
+
+
+pub struct TreeAnalyzer<'a, T> {
+    tree: &'a BST<T>,
+}
+
+impl<'a, T: Ord + std::fmt::Debug> TreeAnalyzer<'a, T> {
+    pub fn new(tree: &'a BST<T>) -> Self {
+        Self { tree }
+    }
+
+    pub fn count_nodes(&self) -> usize {
+        Self::count_rec(&self.tree.root)
+    }
+
+    fn count_rec(node: &Option<Box<Node<T>>>) -> usize {
+        match node {
+            Some(n) => 1 + Self::count_rec(&n.left) + Self::count_rec(&n.right),
+            None => 0,
+        }
+    }
+
+    pub fn height(&self) -> usize {
+        Self::height_rec(&self.tree.root)
+    }
+
+    fn height_rec(node: &Option<Box<Node<T>>>) -> usize {
+        match node {
+            Some(n) => 1 + max(Self::height_rec(&n.left), Self::height_rec(&n.right)),
+            None => 0,
+        }
+    }
+
+    pub fn depth_distribution(&self) -> Vec<usize> {
+        let mut dist = Vec::new();
+        if self.tree.root.is_none() { return dist; }
+
+        let mut queue = VecDeque::new();
+        // В очереди храним кортеж: (ссылка_на_узел, текущая_глубина)
+        queue.push_back((self.tree.root.as_ref().unwrap(), 0));
+
+        while let Some((node, depth)) = queue.pop_front() {
+            if depth >= dist.len() {
+                dist.push(0);
+            }
+            dist[depth] += 1;
+
+            if let Some(left) = &node.left { queue.push_back((left, depth + 1)); }
+            if let Some(right) = &node.right { queue.push_back((right, depth + 1)); }
+        }
+        dist
+    }
+
+    pub fn max_width(&self) -> usize {
+        *self.depth_distribution().iter().max().unwrap_or(&0)
+    }
+
+    pub fn search_with_trace(&self, target: &T) -> bool {
+        println!("Searching for {:?}:", target);
+        let mut current = &self.tree.root;
+
+        while let Some(node) = current {
+            print!("{:?} ", node.value);
+            if *target == node.value {
+                println!("-> FOUND!");
+                return true;
+            } else if *target < node.value {
+                print!("(left) -> ");
+                current = &node.left;
+            } else {
+                print!("(right) -> ");
+                current = &node.right;
+            }
+        }
+        println!("-> NOT FOUND.");
+        false
+    }
+}
